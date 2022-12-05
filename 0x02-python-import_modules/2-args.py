edb6b750aa57e971cf95bc764f65034c63b257f4
vibez24 <jerrywordpressmail@gmail.com>
2022-12-05 14:22:17 +0000

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-import sys
-
-argv = sys.argv[1:]
-argc = len(argv)
-
-txt_arguments = 's' if argc != 1 else ''
-end_string = ':' if argc > 0 else '.'
-
-if __name__ == '__main__':
-    print('{} argument{}{}'.format(argc, txt_arguments, end_string))
-
-    if argc == 0:
-        exit(0)
-
-    for i, arg in enumerate(argv, start=1):
-        print('{}: {}'.format(i, arg))
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) > 2:
+        print("{:d} arguments:".format(len(sys.argv)-1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}" .format(i, sys.argv[i]))
+    elif len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv)-1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {:s}" .format(i, sys.argv[i]))
+    else:
+        print("{:d} arguments.".format(len(sys.argv)-1))
